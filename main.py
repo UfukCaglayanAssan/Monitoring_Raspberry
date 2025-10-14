71efc0abb19b5c931c2263eb41143095cc6656a2
UfukCaglayanAssan <ufuk.caglayan@assanelektronik.com.tr>
2025-10-14 08:21:48 +0300

    ekleme

--- main.py
+++ main.py
@@ -752,7 +752,7 @@ def db_worker():
             if len(data) == 11:
                 arm_value = int(data[3], 16)
                 dtype = int(data[2], 16)
-                k_value = int(data[1], 16)
+                k_value = int(data[1], 16) - 1  # Gelen k değerinden 1 çıkar
                 
                 # 11 byte paket logla
                 print(f"📦 11 BYTE PAKET ALGILANDI")
